73241afa4ce7ad346a2fba706914047907f181e2
jochen ade <jochen.ade@hotmail.com>
2022-11-24 13:43:50 +0100

    custom client audio setup

--- App.js
+++ App.js
@@ -1,20 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import TrackPlayer from 'react-native-track-player';
+import React, { useEffect } from 'react';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+
+TrackPlayer.registerPlaybackService(() => require('./service'));
+
+const tracks = [
+  {
+    id: 1,
+    url: 'https://adagiafiles.s3.eu-west-1.amazonaws.com/audio/60.96a9b6d3-2dd5-43a4-b19a-a60b5c32706a.mp3',
+    title: 'Blues Beat',
+  },
+  {
+    id: 2,
+    url: 'https://adagiafiles.s3.eu-west-1.amazonaws.com/audio/60.96a9b6d3-2dd5-43a4-b19a-a60b5c32706a.mp3',
+    title: 'Blues Beat',
+  },
+];
+
+TrackPlayer.updateOptions({
+  stopWithApp: false,
+  capabilities: [TrackPlayer.CAPABILITY_PLAY, TrackPlayer.CAPABILITY_PAUSE],
+  compactCapabilities: [
+    TrackPlayer.CAPABILITY_PLAY,
+    TrackPlayer.CAPABILITY_PAUSE,
+  ],
+});
+
+const App = () => {
+  const setUpTrackPlayer = async () => {
+    try {
+      await TrackPlayer.setupPlayer();
+      await TrackPlayer.add(tracks);
+      console.log('Tracks added');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    setUpTrackPlayer();
+
+    return () => TrackPlayer.destroy();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.row}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => TrackPlayer.pause()}>
+          <Text style={styles.text}>Pause</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={() => TrackPlayer.play()}>
+          <Text style={styles.text}>Play</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.row}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => TrackPlayer.skipToPrevious()}>
+          <Text style={styles.text}>Prev</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => TrackPlayer.skipToNext()}>
+          <Text style={styles.text}>Next</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  btn: {
+    backgroundColor: '#ff0044',
+    padding: 15,
+    borderRadius: 10,
+    margin: 10,
+    width: 160,
+  },
+  text: {
+    fontSize: 30,
+    color: 'white',
+    textAlign: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 20,
   },
 });
+
+export default App;
